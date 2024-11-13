68abbb1c5df1ead8d0caad4fdb240338daa9eed5
maf1013 <maf1013@alu.ubu.es>
2024-11-13 11:24:28 +0100

    Subida datos ortofosfatos

--- CARGA DE DATOS.R
+++ CARGA DE DATOS.R
@@ -1,25 +1,3 @@
-#FOSFATO EN RIOS EUROPA XML
-library(tidyverse)
-library(XML)
-library(xml2)
-fosfato <- read_xml(x = "INPUT/DATA/fosfato_en_rios.xml")
-
-fosfato_mxl <- xmlParse(fosfato)
-fosfato_mxl
-attributes(fosfato)
-
-DF_fosfato <- xmlToDataFrame(doc = fosfato, stringsAsFactors = FALSE)
-DF_fosfato
-str(DF_fosfato)
-
-
-
-
-
-
-
-
-
 
 #CANCER DE ESTÓMAGO EN EUROPA
 
@@ -35,13 +13,18 @@ cancer_estomago <- read_delim("INPUT/DATA/cancer_estomago.csv", delim = "\"",
 cancer_estomago <- cancer_estomago %>%
   separate(col = ",2005,2006,2007,2008,2009,2010,2011,2012,2013,", 
            into = paste0("Year_", 2005:2013), sep = ",")
-cancer_estomago <- cancer_estomago %>% select(-`...1`, -`...3`, -`...5`, -`,...6`, -`...7`, -`...9`,
+cancer_estomago <- cancer_estomago %>% select(-`Indicator,`,-`...1`, -`...3`, -`...5`, -`,...6`, -`...7`, -`...9`,
                                               -`,...10`, -`...11`, -`...13`, -`,...14`,
                                               -`...15`, -`...17`, -`...19`)
 cancer_estomago <- cancer_estomago %>% slice(-c(247, 248, 249))
 # Visualiza el resultado
 View(cancer_estomago)
 
+
+#DATOS DE ORTOFOSFATO EN RÍOS DE EUROPA
+library(readr)
+rivers_orthophoshate_3 <- read_csv("C:/Users/USUARIO/Desktop/INFORMACION SEMINARIO FUENTES/rivers-orthophoshate-3.csv")
+View(rivers_orthophoshate_3)
 
 
 
